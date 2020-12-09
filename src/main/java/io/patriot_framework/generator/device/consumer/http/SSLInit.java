38bd9d369fb230316f9654a3b91bc6ef5bd56dae
Tomas Ondrusko <tondrusk@redhat.com>
2020-12-09 20:10:20 +0100

    syntax refactor
    pom.xml removed scope, added license to all classes

--- src/main/java/io/patriot_framework/generator/device/consumer/http/SSLInit.java
+++ src/main/java/io/patriot_framework/generator/device/consumer/http/SSLInit.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Patriot project
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package io.patriot_framework.generator.device.consumer.http;
 
 import javax.net.ssl.KeyManagerFactory;
@@ -9,6 +25,7 @@ import java.security.*;
 import java.security.cert.CertificateException;
 
 public class SSLInit {
+
     public static SSLContext createSslContext() throws IOException, KeyStoreException, CertificateException,
             NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
 
