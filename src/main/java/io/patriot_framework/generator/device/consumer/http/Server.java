ae82e96288e21c615898f4ccd6ac59a71d26d6b1
Tomas Ondrusko <tondrusk@redhat.com>
2020-12-09 20:10:20 +0100

    syntax refactor
    pom.xml removed scope, added license to all classes

--- src/main/java/io/patriot_framework/generator/device/consumer/http/Server.java
+++ src/main/java/io/patriot_framework/generator/device/consumer/http/Server.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Patriot project
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package io.patriot_framework.generator.device.consumer.http;
 
 import io.netty.bootstrap.ServerBootstrap;
@@ -20,6 +36,7 @@ import java.nio.channels.UnresolvedAddressException;
 import java.util.List;
 
 public class Server extends AbstractDevice implements Consumer {
+
     private final String serverHost;
     private final int serverPort;
     private final Storage storage;
@@ -72,9 +89,9 @@ public class Server extends AbstractDevice implements Consumer {
             future = bootstrap.bind().sync();
             LOGGER.info(String.format("Started HTTP%s server [PORT: %d]", sslInit != null ? "S" : "", serverPort));
         } catch (UnresolvedAddressException e) {
-            throw new ConsumerException("invalid hostname", e);
+            throw new ConsumerException("Invalid hostname", e);
         } catch (InterruptedException e) {
-            throw new ConsumerException("server was interrupted", e);
+            throw new ConsumerException("Server was interrupted", e);
         }
     }
 
--- src/main/java/io/patriot_framework/generator/device/consumer/mqtt/MqttBuilder.java
+++ src/main/java/io/patriot_framework/generator/device/consumer/mqtt/MqttBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Patriot project
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package io.patriot_framework.generator.device.consumer.mqtt;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
@@ -5,13 +21,13 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import java.util.UUID;
 
 public class MqttBuilder {
+
     private UUID uuid;
     private final String topic;
     private final int id;
     private int qos;
     private boolean isDuplicate;
     private boolean isRetained;
-
     private byte[] payload;
 
     public MqttBuilder(String topic, MqttMessage message) {
