7964919e3dc8ac367f70761c38ab8988eaabf4ad
Jakub <jakub.smadis@gmail.com>
2021-05-08 22:36:30 +0200

    Change ActiveDevice to Active in the runner

--- runner/src/main/java/io/patriot_framework/generator/runner/Runner.java
+++ runner/src/main/java/io/patriot_framework/generator/runner/Runner.java
@@ -2,7 +2,7 @@ package io.patriot_framework.generator.runner;
 
 import io.patriot_framework.generator.controll.server.CoapControlServer;
 import io.patriot_framework.generator.device.Device;
-import io.patriot_framework.generator.device.active.ActiveDevice;
+import io.patriot_framework.generator.device.active.Active;
 import io.patriot_framework.generator.utils.JSONSerializer;
 
 import java.io.File;
@@ -19,14 +19,14 @@ public class Runner {
             String deviceFile = System.getenv(DEVICE_CONFIG_FILE);
             Device device = JSONSerializer.deserializeDevice(new File(deviceFile));
             device.registerToCoapServer();
-            CoapControlServer.getInstance().start();
         }
 
         if (System.getenv(ACTIVE_CONFIG_FILE) != null) {
             String file = System.getenv(ACTIVE_CONFIG_FILE);
-            ActiveDevice activeDevice = JSONSerializer.deserializeActiveDevice(new File(file));
+            Active activeDevice = JSONSerializer.deserializeActiveDevice(new File(file));
             activeDevice.getDevice().registerToCoapServer();
             activeDevice.start();
         }
+        CoapControlServer.getInstance().start();
     }
 }
